18b21da1c7a104c431ac83b290256cdb6afcafce
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-14 17:25:56 -0400

    Implemented Signing up functionalities

---
+++ Elevate/Elevate/View Controllers/SignUpViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/14/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
