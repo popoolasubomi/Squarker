f016b92807dfeb407c8ce5e5a87f699191bc6a68
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-15 12:19:09 -0400

    Project Template

---
+++ Elevate/Elevate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/15/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Elevate/Elevate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/15/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
