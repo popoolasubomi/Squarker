9f7baf77f80599aef51fab2ffd28561b6e697d8c
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-22 15:41:06 -0400

    Setup Home Cell's UI

---
+++ Elevate/Elevate/Views/HomeCell.m
@@ -0,0 +1,24 @@
+//
+//  HomeCell.m
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/22/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import "HomeCell.h"
+
+@implementation HomeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
