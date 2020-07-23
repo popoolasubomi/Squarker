ef81d8628a3c8707e054c845ea6f2340888d9b9e
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-22 21:08:39 -0400

    Added Other Users Profile Screen and View Controller File

---
+++ Elevate/Elevate/View Controllers/UsersProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  UsersProfileViewController.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/22/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UsersProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
