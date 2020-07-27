5f39e3b9ed3e909d1f90e23ac235f702ac252077
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-27 16:13:24 -0400

    Created Progress VC file For Growth Screen

---
+++ Elevate/Elevate/View Controllers/ProgressViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProgressViewController.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/27/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProgressViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Elevate/Elevate/View Controllers/ProgressViewController.m
@@ -0,0 +1,35 @@
+//
+//  ProgressViewController.m
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/27/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import "ProgressViewController.h"
+#import "Elevate-Bridging-Header.h"
+
+@interface ProgressViewController ()
+
+@property (weak, nonatomic) IBOutlet PieChartView *pieChartView;
+
+@end
+
+@implementation ProgressViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
