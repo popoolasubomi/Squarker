de78dd775b315a74ae6a1ddf4919b5f3a907346f
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-08-03 15:58:26 -0400

    Implemented Settings Page For Squat Screen

---
+++ Elevate/Elevate/View Controllers/SquatSettingsViewController.m
@@ -0,0 +1,32 @@
+//
+//  SquatSettingsViewController.m
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 8/3/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import "SquatSettingsViewController.h"
+
+@interface SquatSettingsViewController ()
+
+@end
+
+@implementation SquatSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
