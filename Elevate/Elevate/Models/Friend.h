56b00d38278b380a93a06e3a2c99d4aa23a2c589
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-08-06 15:56:07 -0400

    Created new model claass

---
+++ Elevate/Elevate/Models/Friend.h
@@ -0,0 +1,17 @@
+//
+//  Friend.h
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 8/6/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Friend : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Elevate/Elevate/Models/Friend.m
@@ -0,0 +1,13 @@
+//
+//  Friend.m
+//  Elevate
+//
+//  Created by Ogo-Oluwasobomi Popoola on 8/6/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import "Friend.h"
+
+@implementation Friend
+
+@end
