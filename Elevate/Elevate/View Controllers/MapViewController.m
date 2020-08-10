c3c71c75980f6940182c5cc010ef1738e4472a04
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-08-10 04:34:08 -0400

    Activated functions in map view controller

--- Elevate/Elevate/View Controllers/MapViewController.m
+++ Elevate/Elevate/View Controllers/MapViewController.m
@@ -22,7 +22,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self getCurrentLocation];
+    [self createMapView];
 }
 
 - (void) getCurrentLocation{
